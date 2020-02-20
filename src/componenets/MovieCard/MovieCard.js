72a0aa540f979f996b8e58026bc3d1404141394d
Noah Gambee <noah@gambee.dev>
2020-02-20 15:35:25 -0700

    Add imports and boilerplate set up

---
+++ src/componenets/MovieCard/MovieCard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+export const MovieCard = ({title, posterPath, id}) => {
+  return (
+    <section className='movie-card'>
+      <h2 className='movie-card-title'>{title}</h2>
+      
+    </section>
+  )
+}
