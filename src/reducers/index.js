f7271a093014351645b900e409fa5d1cdbc28eb0
Noah Gambee <noah@gambee.dev>
2020-02-20 08:48:36 -0700

    Add loading reducer into root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { movieReducer } from './movies.js';
 import { combineReducers } from 'redux';
+import { loadingReducer } from './loading.js'
 
 // Purpose: to export a single reducer function called Root Reducer
 
 const rootReducer = combineReducers({
-  movies: movieReducer
+  movies: movieReducer,
+  loadingStatus: loadingReducer
 })
 
 export default rootReducer;
