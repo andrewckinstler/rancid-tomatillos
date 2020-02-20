334446847a62aafbd7765c50cfbc45973c5911bf
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-20 16:34:54 -0700

    Add Post action to case

--- src/reducers/ratings.js
+++ src/reducers/ratings.js
@@ -2,6 +2,8 @@ export const ratingsReducer = (state = [], action) => {
   switch (action.type) {
     case 'GET_RATINGS':
       return [...action.ratings];
+    case 'POST_RATING':
+      return [...action.ratings, action.rating]
     default:
       return state;
   }
