4912a9c5f7c91e81e0e7ff42badbc0f690ef086c
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-20 15:40:37 -0700

    Add ratings into rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { movieReducer } from './movies.js';
 import { combineReducers } from 'redux';
-import { loadingReducer } from './loading.js'
+import { loadingReducer } from './loading.js';
+import { ratingsReducer } from './ratings.js';
 
 // Purpose: to export a single reducer function called Root Reducer
 
 const rootReducer = combineReducers({
   movies: movieReducer,
+  ratings: ratingsReducer,
   loadingStatus: loadingReducer
 })
 
