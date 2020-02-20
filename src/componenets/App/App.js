68f6f1a8bf53d6b8eca328c95e118d17868cb37a
Noah Gambee <noah@gambee.dev>
2020-02-20 08:47:46 -0700

    Move movie card data management out of App

--- src/componenets/App/App.js
+++ src/componenets/App/App.js
@@ -2,45 +2,28 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.scss';
-import { fetchMovies } from '../../actions';
+import { fetchMovies, getMovies } from '../../actions';
+import { fetchMoviesAPI } from '../../apiCalls/apiCalls.js'
+import MovieContainer from '../../containers/MovieContainer/MovieContainer.js'
 
 class App extends Component {
   constructor() {
     super();
-    // this.state
+    this.state = {};
   }
 
   componentDidMount() {
-    this.props.dispatch(fetchMovies())
   }
 
   render() {
-    const { error, loading, movies } = this.props;
-
-    if (error) {
-      console.log(error);
-    }
-
-    if (loading) {
-      console.log(loading);
-    }
-    
-    // this.props.movies.movies.map(movie => {
-    //   return <h2>{movie.title}</h2>
-    // })
     return (
       <div>
         <h1>Oh Hi There...</h1>
-        <h2>{this.props.movies.movies[0].title}</h2>
+        <MovieContainer />
+        <h2>{ 'fewhfiuweh'}</h2>
       </div>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  movies: state.movies.movies,
-  loading: state.movies.loading,
-  error: state.movies.error
-})
-
-export default connect(mapStateToProps)(App);
+export default App;
