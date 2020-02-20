70af23adc7e4f4ec0f31c82fd6a95a7adbc22ede
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-20 16:33:02 -0700

    Import MovieDetail component

--- src/componenets/App/App.js
+++ src/componenets/App/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.scss';
-import { fetchMovies, getMovies } from '../../actions';
-import { fetchMoviesAPI } from '../../apiCalls/apiCalls.js'
 import MovieContainer from '../../containers/MovieContainer/MovieContainer.js'
+import MovieDetail from '../MovieDetail/MovieDetail.js';
 
 class App extends Component {
   constructor() {
@@ -12,14 +11,12 @@ class App extends Component {
     this.state = {};
   }
 
-  componentDidMount() {
-  }
-
   render() {
     return (
       <div>
         <h1>Oh Hi There...</h1>
         <MovieContainer />
+        {/* <MovieDetail /> */}
         <h2>{ 'fewhfiuweh'}</h2>
       </div>
     )
