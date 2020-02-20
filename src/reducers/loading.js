614b86dd47eb8531b45a2221936245f2a89dfa7a
Noah Gambee <noah@gambee.dev>
2020-02-20 08:46:55 -0700

    Add loading reducer to handle loading action

---
+++ src/reducers/loading.js
@@ -0,0 +1,9 @@
+export const loadingReducer = (state = true, action) => {
+  switch(action.type) {
+    case 'LOADING_MOVIES':
+      return action.loadingStatus;
+    default:
+      return state;
+  }
+
+}
