2976198fd465d7ec09ea66f3a749029d13566578
Noah Gambee <noah@gambee.dev>
2020-02-22 14:49:59 -0700

    Style background image and display movie details in a more visually pleasing way

--- src/componenets/MovieDetail/MovieDetail.js
+++ src/componenets/MovieDetail/MovieDetail.js
@@ -13,16 +13,19 @@ class MovieDetail extends Component {
 
   render() {
     let {title, id, average_rating, poster_path, backdrop_path, release_date, overview} = this.props.selectedMovie
+    const backgroundStyling = {
+      background: `url(${backdrop_path}) no-repeat center center fixed`,
+      backgroundSize: 'cover',
+    }
     return (
-      <section className='movie-detail'>
-        <div className='movie-detail__info'>
+      <section className='movie-detail' style={backgroundStyling}>
+        <div className='movie-detail__info' >
           <h2 className='movie-detail__title'>{title}</h2>
           <p>{`Average Rating: ${average_rating}`}</p>
           <label for='user-rating'>User Rating:</label>
           <input type='range' min='1' max='10' id='user-rating' name='user-rating' />
           <p>{overview}</p>
         </div>
-        <image className='movie-detail__image' url=''></image>
       </section>
     )
   }
