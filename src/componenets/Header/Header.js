f735d0e97c50d88dc5533f3944c6fa3a5d33ab93
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-24 13:41:36 -0700

    Change class name

--- src/componenets/Header/Header.js
+++ src/componenets/Header/Header.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return(
- <article className="header_main-container">
-   <article className="header_welcome-text">
-     <h1>Welcome, [user]!</h1>
-     <h3> Avg. Rating: 5.5 </h3>
-     <h3>Number of Ratings:</h3>
-   </article>
-   <Link to={'/'} className="header_main-link">
-     Rancid Tomatillos
-   </Link>
-   <button className="header_logoff-button">Log Off</button>
- </article>
- )
+    <article className="header_main-container">
+      <article className="header_welcome-text">
+        <h1>Welcome, [user]!</h1>
+        <h3>Avg. Rating: 5.5</h3>
+        <h3>Number of Ratings:</h3>
+      </article>
+      <Link to={'/'} className="header_main-link">
+        Rancid Tomatillos
+      </Link>
+      <button className="header_log-button">Log Off</button>
+    </article>
+  )
 }
 
 export default Header;
