c744d1564b0fe7508a3c133d56827082f7199068
Andrew Kinstler <andrewckinstler@gmail.com>
2020-02-24 12:07:41 -0700

    Adds conditional rendering for when there is and is not a user

--- src/componenets/Header/Header.js
+++ src/componenets/Header/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => {
-	return(
- <article className="header_main-container">
-   <article className="header_welcome-text">
-     <h1>Welcome, [user]!</h1>
-     <h3> Avg. Rating: 5.5 </h3>
-     <h3>Number of Ratings:</h3>
-   </article>
-   <Link to={'/'} className="header_main-link">
-     Rancid Tomatillos
-   </Link>
-   <button className="header_logoff-button">Log Off</button>
- </article>
- )
+const Header = (props) => {
+  console.log(props)
+	return (
+    <article className="header_main-container">
+      {!props.user 
+      ? null
+    : <article className="header_welcome-text">
+    <h1>Welcome, [user]!</h1>
+    <h3> Avg. Rating: 5.5 </h3>
+    <h3>Number of Ratings:</h3>
+  </article>}
+      <Link to={'/'} className="header_main-link">
+        Rancid Tomatillos
+      </Link>
+      {!props.user 
+      ? <Link to={'/login'} className="header_logoff-button">Login</Link>
+      :<Link to={'/'} className="header_logoff-button">Logout</Link>}
+    </article>
+  )
 }
 
-export default Header;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(Header);
