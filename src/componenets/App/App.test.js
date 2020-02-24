3988c42751d5bdd60cf79aa805860f1205734114
Noah Gambee <noah@gambee.dev>
2020-02-24 11:52:10 -0700

    Add describe block and tests for mapDispatch within app component

--- src/componenets/App/App.test.js
+++ src/componenets/App/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { loadingMovies, getMovies, getRatings } from '../../actions/index.js'
 import { shallow } from 'enzyme';
 import { App, mapStateToProps, mapDispatchToProps } from './App.js';
 
@@ -28,7 +29,34 @@ describe('App', () => {
       expect(mappedProps).toEqual(expected)
     })
   })
-  // describe('mapDispatchToProps', () => {
-  //   it('should call dispatch with a postRating action when ')
-  // })
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with a loadingMovies action when loadingMovies is called', () => {
+      const mockDispatch = jest.fn();
+      const action = loadingMovies(true);
+      const mappedProps = mapDispatchToProps(mockDispatch)
+
+      mappedProps.loadingMovies(true);
+      expect(mockDispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should call dispatch with a getMovies action when getMovies is called', () => {
+      const mockDispatch = jest.fn();
+      const movies = [{title: 'Parasite', rating: 9}, {title: '1917', rating: 8}]
+      const action = getMovies(movies);
+      const mappedProps = mapDispatchToProps(mockDispatch)
+
+      mappedProps.getMovies(movies);
+      expect(mockDispatch).toHaveBeenCalledWith(action);
+    })
+
+    it('should call dispatch with a getRatings action when getRatings is called', () => {
+      const mockDispatch = jest.fn();
+      const ratings = [{id: 25, rating: 9}, {id: 27, rating: 4}];
+      const action = getRatings(ratings);
+      const mappedProps = mapDispatchToProps(mockDispatch)
+
+      mappedProps.getRatings(ratings);
+      expect(mockDispatch).toHaveBeenCalledWith(action);
+    })
+  })
 })
