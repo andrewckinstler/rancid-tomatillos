8975ef5d9d2b2ddd8f4720ed008bba82d396e462
Noah Gambee <noah@gambee.dev>
2020-02-24 11:44:17 -0700

    Add additional prop to test

--- src/componenets/App/App.test.js
+++ src/componenets/App/App.test.js
@@ -12,18 +12,23 @@ describe('App', () => {
   })
 
   describe('mapStateToProps', () => {
-    it('should return an object with the movies and ratings array', () => {
+    it('should return an object with the movies and ratings array and a loading status', () => {
       const mockState = {
         movies: [{title: 'Parasite', rating: 9}, {title: '1917', rating: 8}],
         ratings: [{id: 25, rating: 9}, {id: 27, rating: 4}],
-        user: {id: 123, name: 'me'}
+        user: {id: 123, name: 'me'},
+        loadingStatus: true
       }
       const expected = {
         movies: [{title: 'Parasite', rating: 9}, {title: '1917', rating: 8}],
-        ratings: [{id: 25, rating: 9}, {id: 27, rating: 4}]
+        ratings: [{id: 25, rating: 9}, {id: 27, rating: 4}],
+        loadingStatus: true
       }
       const mappedProps = mapStateToProps(mockState)
       expect(mappedProps).toEqual(expected)
     })
   })
+  // describe('mapDispatchToProps', () => {
+  //   it('should call dispatch with a postRating action when ')
+  // })
 })
