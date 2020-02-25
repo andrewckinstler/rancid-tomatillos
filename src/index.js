074d4b371d20441a849c06e36affd5f46efea584
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-25 14:58:45 -0700

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import './index.css';
 import App from './componenets/App/App.js';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const store = createStore(rootReducer, composeWithDevTools());
 
