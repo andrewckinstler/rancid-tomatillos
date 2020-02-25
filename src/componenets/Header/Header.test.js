4a3796670242c6ad7ee2c2c73911979d7bb9aad4
Noah Gambee <noah@gambee.dev>
2020-02-25 10:50:21 -0700

    Add MSTP test and update snapshot

--- src/componenets/Header/Header.test.js
+++ src/componenets/Header/Header.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from './Header.js';
+import { Header, mapStateToProps, mapDispatchToProps } from './Header.js';
 
 describe('Header', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<Header />)
+    wrapper = shallow(<Header
+      ratings = {[{rating: 2}, {rating: 3}]}
+       />)
   })
   it('should match a snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
-
-// need mapState and mapDispatch tests here
+  describe('mapStateToProps', () => {
+    it('should return an object with the user and ratings array', () => {
+      const mockState = {
+        movies: [{title: 'Parasite', rating: 9}, {title: '1917', rating: 8}],
+        ratings: [{id: 25, rating: 9}, {id: 27, rating: 4}],
+        user: {id: 123, name: 'me'},
+        loadingStatus: true,
+        someOtherProp: false
+      }
+      const expected = {
+        ratings: [{id: 25, rating: 9}, {id: 27, rating: 4}],
+        user: {id: 123, name: 'me'},
+      }
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
 })
